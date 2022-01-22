aa3becd8994e86d4ebaf501a2459bd7c4ffab59b
甚平(郭建强) <guojianqiang@i-i.ai>
2022-01-22 22:00:48 +0800

    docs: update docs

--- build/index.d.ts
+++
@@ -1,4 +0,0 @@
-import 'reflect-metadata';
-export declare const inject: () => (target: any, key: any, descriptor?: any) => any;
-export declare const injectable: () => (target: any) => any;
-export declare const useService: <T extends object>(Service: T) => T;
